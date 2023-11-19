d0366cab191e55de43cae5ddfe97530da72ecf5d
tungstengmd <gostdud@gmail.com>
2023-11-19 12:28:37 +0000

    Test if this works

---
+++ test.js
@@ -0,0 +1,7 @@
+for (let index = 0; index < 4; index++) {
+    var number
+    const element = array["Enter a number", "The total is " + number];
+    number = window.prompt(array[1])
+    total = number + total
+}
+alert(array[2])
