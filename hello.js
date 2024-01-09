d0c43b4aee0f5c6490500816908d3f1b83721eb5
tungstengmd <gostdud@gmail.com>
2024-01-09 12:33:28 +0000

    Stage an unstaged change

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
 document.write ("Hello World!")
-var mood
 mood = window.prompt("How are you?")
-var reason = window.prompt("Why are you feeling" + mood + "?")
+reason = window.prompt("Why are you feeling" + mood + "?")
 if (mood.toLowerCase == "sad" || mood.toLowerCase == "angry") {
     alert("damn, sucks to suck HAHAHAHAHAHA")
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
