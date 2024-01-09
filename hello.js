327c10dc7f073b794029afaecc7fa06e0d0b47b4
tungstengmd <gostdud@gmail.com>
2024-01-09 12:33:06 +0000

    Remove redundant "var" declarations again
    Stage an unstaged change

--- hello.js
+++ hello.js
@@ -1,10 +1,6 @@
-//Program: Hello World
-//Author Date: Benyamin Karolia, 11/09/2023
-
 document.write ("Hello World!")
-var mood
 mood = window.prompt("How are you?")
-var reason = window.prompt("Why are you feeling" + mood + "?")
+reason = window.prompt("Why are you feeling" + mood + "?")
 if (mood.toLowerCase == "sad" || mood.toLowerCase == "angry") {
     alert("damn, sucks to suck HAHAHAHAHAHA")
     window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
