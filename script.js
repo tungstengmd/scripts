7ac89f6b8495f0bb59b5d6d40dacfa608322c4a7
tungstengmd <118553985+tungstengmd@users.noreply.github.com>
2024-03-13 12:58:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay)); //IT'S 9PM GO TO SLEEP 
-console.log("The scripts are: the multiplier, the one that states your favourite colour, the name swapper, the little test I made, the weekly pay calculator, the address formatter, the Uber ® fare calculator, my while loop test and the number detector.") 
 sleep(1) 
-startScript = prompt("Which script should be started? Answer with 1 to 9 respectively. Check the console for the script list."); 
+startScript = prompt("Which script should be started? Answer with 1 to 9 respectively. The scripts are: the multiplier, the one that states your favourite colour, the name swapper, the little test I made, the weekly pay calculator, the address formatter, the Uber ® fare calculator, my while loop test and the number detector."); 
 switch (startScript) {
     case "1":
         number1 = prompt("What number do you wish to multiply?"); 
