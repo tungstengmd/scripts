a23e29f9aa347636e6634a0a2202f4834ac7f3c9
tungstengmm <gostdud@gmail.com>
2024-05-10 12:26:51 +0100

    A couple fixes

--- script.js
+++ script.js
@@ -63,8 +63,7 @@ switch (startScript) {
         break;
     case "9":
         number = prompt("Gimme a number.");
-        if (number % 2 == 0) { //the percent sign sends the remainder back with modulus
-            alert("That's an even number or smth idk i forgor mdgkjbhdsmbj");
+        if (number % 2 == 0) { //the percent sign sends the remainder back with modulus            alert("That's an even number or smth idk i forgor mdgkjbhdsmbj");
         } else if (number % 2 == 1) {
             alert("you are one silly oddball. Or are you, perhaps, an odd sphere? *vsauce theme plays*");
         }
