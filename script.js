be312b7c486861da65136134e2293a5b10551eaf
tungstengmd <gostdud@gmail.com>
2024-06-10 10:15:59 +0100

    Oh me? I'm just *diff*

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ switch (startScript) {
     case "9":
         number = prompt("Gimme a number.");
         if (number % 2 == 0) { //the percent sign sends the remainder back with modulus
-            alert("That's an even number or smth idk i forgor mdgkjbhdsmbj");
+            alert("That's an even number or smth idk i forgor mdgkjbhdsmbj :3");
         } else if (number % 2 == 1) { //omg line 69 :O
             alert("You are one silly oddball. Or are you, perhaps, an odd sphere? *vsauce theme plays*");
         }
