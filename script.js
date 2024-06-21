0535a4f4e742610413cbb94c9b795568938b8414
tungstengmd <gostdud@gmail.com>
2024-06-21 10:40:57 +0100

    Few more fixes

--- script.js
+++ script.js
@@ -81,16 +81,19 @@ switch (startScript) {
         func = parseInt(prompt("What function shall be performed? Answer from 1 to 4. The functions are addition, subtraction, division and multiplication."));
         number1 = parseInt(prompt("What is the first number?"));
         number2 = parseInt(prompt("What is the second number?"));
-        if (func == 1) {
+        switch (func) {
+          case 1:
             alert(`${number1} + ${number2} = ${number1+number2}`);
-        } else if (func == 2) {
+            break;
+          case 2:
             alert(`${number1} - ${number2} = ${number1-number2}`);
-        } else if (func == 3) {
+            break;
+          case 3:
             alert(`${number1} / ${number2} = ${number1/number2}`);
-        } else {
+            break;
+          default:
             alert(`${number1} * ${number2} = ${number1*number2}`);
-        }
-        break;
+            break;
     default:
         alert("You need to pick one. Let's try again, shall we?");
         location.reload(); //do i need to elaborate?
